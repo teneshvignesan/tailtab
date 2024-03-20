be4825497675fba944f57b232af2f1078bf3050d
Tenesh Vignesan <teneshdev@gmail.com>
2024-03-21 00:01:20 +0800

    WIP

--- src/lib/component/authenticated/Railbar.svelte
+++ src/lib/component/authenticated/Railbar.svelte
@@ -32,10 +32,7 @@
 		? ''
 		: 'hidden'} absolute top-0 left-0 h-full w-full xl:relative xl:flex xl:w-[15rem] z-10">
 	<aside class="flex flex-col bg-white border-r border-gray-200 h-full w-[15rem]">
-		<a
-			href="/"
-			aria-label="Tailtab"
-			class=" flex items-center h-20 px-4 py-3 sm:px-6 xl:px-8 bg-blue-50">
+		<a href="/" aria-label="Tailtab" class=" flex items-center h-20 px-4 py-3 sm:px-6 xl:px-8">
 			<img src="/favicon.png" alt="Tailtab logo" class="w-7" />
 		</a>
 		<nav class="flex flex-col w-full space-y-5 justify-center items-center px-6 py-4">
